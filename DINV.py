2b50cb586b9190e2ddc4d0933d2973647de46e09
Jalen Cates <jmcates@crimson.ua.edu>
2019-06-27 13:16:53 -0700

    DINV complete?

--- DINV.py
+++ DINV.py
@@ -6,6 +6,8 @@ import time
 
 import tensorflow as tf
 
+import ZMCintegral
+
 N=3
 mu = 0.1  # Fermi-level
 hOmg = 0.3  # Photon energy eV
@@ -31,11 +33,10 @@ cent = np.arange(-(N - 1) / 2, (N - 1) / 2 + 1, 1)
 # inx = np.linalg.inv(x)
 
 # res = tf.matmul(inx, x)
+qy = 0
 
-def heaviside(tensor, arg)
 
-
-def ds(kx, ky, qx, qy, om):
+def ds(kx, ky, qx, om):
     topkq = -V0 * ( (ky + qy) + complex(0, (kx + qx)) )
     botkq = -V0 * ( (ky + qy) - complex(0, (kx + qx)) )
     innkq = om + complex(0, Gamm) - A * ((kx + qx) ** 2 + (ky + qy) ** 2) - V2
@@ -49,19 +50,47 @@ def ds(kx, ky, qx, qy, om):
     #Ginkq = np.eye(N, N, k=1) * topkq + np.eye(N, N, k=-1) * botkq + innkq * np.eye(N, N) - d
     #Gink = np.eye(N, N, k=1) * topk + np.eye(N, N, k=-1) * botk + innk * np.eye(N, N) - d
     
-    Ginkq = tf.matrix_diag(topkq, k=1) + tf.matrix_diag(botkq, k=-1) + tf.matrix_diag(innkq) - d
-    Gink = tf.matrix_diag(topk, k=1) + tf.matrix_diag(botk, k=-1) + tf.matrix_diag(innk) - d
+    # Identity
+    z=tf.Variable(tf.eye(N,N,[len(kx)],dtype=tf.complex64))
+
+    # Lower Diag
+    xz=tf.cast(tf.zeros_like(kx),dtype=tf.complex64)
+    zdp=tf.Variable(tf.roll(z,1,1))
+    zd=zdp[:,0,2].assign(xz)
+
+    # Upper Diag
+    ztp=tf.Variable(tf.roll(z,-1,1))
+    zt=ztp[:,2,0].assign(xz)
+
+    Ginkq=tf.transpose(tf.multiply(tf.transpose(z,perm=[2,1,0]),innkq),perm=[2,0,1]) \
+        + tf.transpose(tf.multiply(tf.transpose(zd,perm=[2,1,0]),botkq),perm=[2,1,0]) \
+        + tf.transpose(tf.multiply(tf.transpose(zt,perm=[2,1,0]),topkq),perm=[2,1,0]) \
+        - d
+
+    Gink=tf.transpose(tf.multiply(tf.transpose(z,perm=[2,1,0]),innk),perm=[2,0,1]) \
+            + tf.transpose(tf.multiply(tf.transpose(zd,perm=[2,1,0]),botk),perm=[2,1,0]) \
+            + tf.transpose(tf.multiply(tf.transpose(zt,perm=[2,1,0]),topk),perm=[2,1,0]) \
+            - d
+
+    #Ginkq = tf.matrix_diag(topkq, k=1) + tf.matrix_diag(botkq, k=-1) + tf.matrix_diag(innkq) - d
+    #Gink = tf.matrix_diag(topk, k=1) + tf.matrix_diag(botk, k=-1) + tf.matrix_diag(innk) - d
 
     Grkq = tf.matrix_inverse(Ginkq)
-    Gakq = tf.matrix_transpose(Grkq, conjugate=True)
+    #Gakq = tf.matrix_transpose(Grkq, conjugate=True)
 
     Grk = tf.matrix_inverse(Gink)
-    Gak = tf.matrix_transpose(Grk, conjugate=True)
+    #Gak = tf.matrix_transpose(Grk, conjugate=True)
 
-    fer = np.heaviside(-(d + np.eye(N, N) * (om - mu)), 0)
+    #fer = np.heaviside(-(d + np.eye(N, N) * (om - mu)), 0)
 
-    in1 = tf.matmul(Grkq, tf.matmul(Grk, tf.matmul(fer, Gak)))
-    in2 = tf.matmul(Grkq, tf.matmul(fer, tf.matmul(Gakq, Gak)))
+    cond = tf.less(om-mu, 0)
+    muom = tf.where(cond, xz, om-mu)
+
+    fer = tf.transpose(tf.multiply(tf.transpose(z,perm=[2,1,0]),muom),perm=[2,1,0]) \
+            - d
+
+    in1 = tf.matmul(Grkq, tf.matmul(Grk, tf.matmul(fer, Grk, adjoint_b=True)))
+    in2 = tf.matmul(Grkq, tf.matmul(fer, tf.matmul(Grkq, Grk, adjoint_a=True, adjoint_b=True)))
     tr = np.trace(in1 + in2)
     # HERE i will divide by DOS, multiply by 2 for spin, and divide by (2pi)^3
 
@@ -69,8 +98,24 @@ def ds(kx, ky, qx, qy, om):
 
     return dchi
 
+
+
+myconfig = tf.ConfigProto(log_device_placement=True)
+myconfig.gpu_options.allow_growth = True
+session = tf.Session(config=myconfig)
+
+def testfoo(x):
+    return tf.sin(x[0]+x[1]+x[2]+x[3])
+
+MC = ZMCintegral.MCintegral(ds, [[0,1], [0,1], [0,1], [0,1]], available_GPU=[0])
+
 start = time.time()
-print('ds = ', ds(0.1, 0.1, 0.01, 0, 0.09))
+result = MC.evaluate()
 end = time.time()
-print('Time :', end - start)
+
+print('=====================================')
+print('Result = ', result[0])
+print(' Error = ', result[1])
+print('=====================================')
+print('Time = ', end-start, 'seconds')
 
